77475f4f97166bef548e74e81c0d3600ee2ec71c
sojack <sojack@gmail.com>
2020-04-01 09:54:40 -0400

    update gatsby config and Readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `TLA Graphics`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `2020 version of tlagraphics.com`,
+    author: `@tlagraphics`,
   },
   pathPrefix: `/2020-a`,
   plugins: [
@@ -22,10 +22,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#00a19b`,
+        theme_color: `#00a19b`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/tla-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
