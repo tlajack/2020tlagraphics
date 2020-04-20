f8ed308d7a7474efe7c86323e63be032713f5a11
sojack <sojack@gmail.com>
2020-04-20 18:09:17 -0400

    start laying out the blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,80 +1,93 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const BlogPage = () => (
+import "../components/blog.css"
+
+const BlogEntry = props => {
+  const additionalStyles = props.featured ? "featuredEntry" : ""
+  const allStyles = "blogEntry" + " " + additionalStyles
+  return (
+    <div className={allStyles}>
+      <Img
+        fluid={props.blogImage.childImageSharp.fluid}
+        alt="alt text tk"
+        className="blogImage"
+      />
+      <div>
+        <h2>
+          <Link to={props.blogSlug}>{props.blogTitle}</Link>
+        </h2>
+        <p className="blogDate">{props.blogDate}</p>
+        <p>
+          {props.blogIntro} <Link to={props.blogSlug}> >> read more</Link>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+const BlogPage = ({ data }) => (
   <Layout>
     <SEO title="Blog" />
     <h1>Blog</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel est
-      convallis, ultricies quam id, semper urna. Sed quis auctor tellus.
-      Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-      ac turpis egestas. Donec nec risus sed arcu congue condimentum at non
-      urna. Donec massa elit, laoreet dapibus nulla et, volutpat imperdiet urna.
-      Quisque tincidunt, metus sed vestibulum suscipit, felis diam faucibus
-      urna, sit amet commodo nibh turpis at odio. Duis lacinia accumsan libero.
-      Sed sit amet arcu finibus, venenatis nunc id, sodales est. Duis vehicula
-      dolor arcu, nec pretium nibh ultrices eu. Curabitur non dapibus augue. Sed
-      efficitur justo sit amet erat rhoncus, in eleifend massa accumsan.
-    </p>
-
-    <p>
-      Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-      ac turpis egestas. Duis neque tellus, finibus non tincidunt vel, hendrerit
-      sit amet libero. Ut vestibulum lectus eu nibh mattis, quis finibus dui
-      placerat. Proin ex lacus, pulvinar vel viverra ac, laoreet vel dolor. Cras
-      quis tristique lorem. Nulla non mauris non est consectetur mollis eu eu
-      nisi. Sed scelerisque, nibh non pellentesque pharetra, magna est ultricies
-      lectus, sit amet congue nibh lectus id ipsum. Donec convallis bibendum
-      egestas. Maecenas eget diam nibh. Quisque non nibh mattis elit ultricies
-      sollicitudin sed non felis.
-    </p>
-
-    <p>
-      Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-      ac turpis egestas. Fusce posuere, est eget gravida egestas, nisi dolor
-      molestie nunc, et luctus velit ex eu urna. Suspendisse aliquet aliquet
-      turpis nec consectetur. Donec arcu sapien, ultricies in molestie commodo,
-      eleifend sed orci. Vivamus posuere sem sem, sed malesuada nisl mollis at.
-      Nunc eget pretium risus. Donec ut tortor ut diam viverra tincidunt. Aenean
-      vel nisl dignissim, porta lacus vitae, condimentum eros. Nullam fermentum
-      tincidunt pharetra. Nam cursus scelerisque est, nec imperdiet nunc
-      volutpat eget. Phasellus pulvinar, dolor ac congue suscipit, neque mauris
-      volutpat tellus, non pellentesque dolor augue sed mi. Cras ac commodo est.
-      Donec laoreet ullamcorper augue quis iaculis. Mauris tempor quis tortor
-      vel commodo.
-    </p>
-
-    <p>
-      Etiam efficitur, nisi sit amet dictum sollicitudin, lacus justo lobortis
-      eros, et dapibus quam augue vitae diam. Nunc euismod eleifend ultrices.
-      Vestibulum ac tellus sagittis, viverra mi ut, fermentum magna. Nunc ornare
-      laoreet diam. Suspendisse cursus pretium justo, at fermentum tortor
-      accumsan ut. Aenean in vestibulum velit. Curabitur pretium nunc vitae
-      aliquet tincidunt. In elementum, diam eget luctus varius, elit enim
-      fringilla orci, quis porta enim nibh accumsan ante. Donec fringilla lorem
-      sit amet velit sagittis, nec euismod eros porttitor. Pellentesque varius
-      nec nisl non rhoncus. Duis pharetra diam scelerisque, efficitur magna non,
-      vehicula justo. Suspendisse potenti. Donec ex tellus, mattis non libero
-      eu, mattis hendrerit arcu.
-    </p>
-
-    <p>
-      Etiam erat odio, viverra at nulla ac, elementum fermentum augue. Nulla id
-      nulla nec mauris malesuada accumsan. Etiam scelerisque, ex eu laoreet
-      placerat, justo tortor cursus lorem, pharetra dapibus urna magna eget
-      turpis. Praesent pulvinar risus eget tellus fringilla, et pellentesque
-      lorem dapibus. Nulla aliquam tortor ac quam mattis venenatis. Nullam
-      sagittis, mauris sit amet rutrum sodales, quam lacus ultrices lorem, eget
-      volutpat diam odio et enim. Duis quis lectus est. Vivamus bibendum elit
-      est, ac mollis dolor consequat consectetur. Maecenas euismod sit amet elit
-      eget molestie. Nullam eu sem posuere, venenatis nulla in, convallis nibh.
-      Proin vitae purus augue.
-    </p>
+    <section className="blogContainer">
+      <BlogEntry
+        blogDate="2020-02-05"
+        blogTitle="Latest Article Title"
+        blogIntro="Duis lacinia accumsan libero. Sed sit amet arcu finibus, venenatis nunc id, sodales est. Duis vehicula dolor arcu, nec pretium nibh ultrices eu. Curabitur non dapibus augue. Sed efficitur justo sit amet erat rhoncus, in eleifend massa accumsan."
+        blogSlug="20_02_05-article01"
+        blogImage={data.placeholder}
+        featured={true}
+        className="blogEntry"
+      />
+      <BlogEntry
+        blogDate="2020-02-05"
+        blogTitle="Title 02"
+        blogIntro="Duis lacinia accumsan libero. Sed sit amet arcu finibus, venenatis nunc id, sodales est. Duis vehicula dolor arcu, nec pretium nibh ultrices eu. Curabitur non dapibus augue. Sed efficitur justo sit amet erat rhoncus, in eleifend massa accumsan."
+        blogSlug="20_02_05-article02"
+        blogImage={data.placeholder}
+        className="blogEntry"
+      />
+      <BlogEntry
+        blogDate="2020-02-05"
+        blogTitle="Title 03"
+        blogIntro="Duis lacinia accumsan libero. Sed sit amet arcu finibus, venenatis nunc id, sodales est. Duis vehicula dolor arcu, nec pretium nibh ultrices eu. Curabitur non dapibus augue. Sed efficitur justo sit amet erat rhoncus, in eleifend massa accumsan."
+        blogSlug="20_02_05-article03"
+        blogImage={data.placeholder}
+        className="blogEntry"
+      />
+      <BlogEntry
+        blogDate="2020-02-05"
+        blogTitle="Title 04"
+        blogIntro="Duis lacinia accumsan libero. Sed sit amet arcu finibus, venenatis nunc id, sodales est. Duis vehicula dolor arcu, nec pretium nibh ultrices eu. Curabitur non dapibus augue. Sed efficitur justo sit amet erat rhoncus, in eleifend massa accumsan."
+        blogSlug="20_02_05-article04"
+        blogImage={data.placeholder}
+        className="blogEntry"
+      />
+      <BlogEntry
+        blogDate="2020-02-05"
+        blogTitle="Title 05"
+        blogIntro="Duis lacinia accumsan libero. Sed sit amet arcu finibus, venenatis nunc id, sodales est. Duis vehicula dolor arcu, nec pretium nibh ultrices eu. Curabitur non dapibus augue. Sed efficitur justo sit amet erat rhoncus, in eleifend massa accumsan."
+        blogSlug="20_02_05-article05"
+        blogImage={data.placeholder}
+        className="blogEntry"
+      />
+    </section>
   </Layout>
 )
-
+export const query = graphql`
+  query {
+    placeholder: file(relativePath: { eq: "tla-icon.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 200) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 export default BlogPage
