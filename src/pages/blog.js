98261f2e4a9d93e9fc8bc796f92f660c875d7ea7
sojack <sojack@gmail.com>
2020-04-23 20:28:20 -0400

    fix warning  Unexpected string concatenation of literals

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -9,7 +9,7 @@ import "../components/blog.css"
 
 const BlogEntry = props => {
   const additionalStyles = props.featured ? "featuredEntry" : ""
-  const allStyles = "blogEntry" + " " + additionalStyles
+  const allStyles = `blogEntry ${additionalStyles}`
   return (
     <div className={allStyles}>
       <Img
