97d11041d039b58e5437e940e51cfa83fc17eee0
sojack <sojack@gmail.com>
2020-04-29 16:17:26 -0400

    change body typeface to Open Sans and headers to Proza Libre - both google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,22 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Proza Libre`,
+            // subsets: [`latin`],
+            variants: [`700`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`],
+          },
+        ],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
