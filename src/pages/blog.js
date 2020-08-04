c081d8342112dfabeb7d558fe159e3dd78d09c59
tlaJack <jack@tlagraphics.com>
2020-08-04 17:47:56 -0400

    update blog page to css modules

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,23 +5,23 @@ import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import "../components/blog.css"
+import styles from "../components/blog.module.css"
 
 const BlogEntry = props => {
-  const additionalStyles = props.featured ? "featuredEntry" : ""
-  const allStyles = `blogEntry ${additionalStyles}`
+  const additionalStyles = props.featured ? `${styles.featuredEntry}` : ""
+  const allStyles = `${styles.blogEntry} ${additionalStyles}`
   return (
     <div className={allStyles}>
       <Img
         fluid={props.blogImage.childImageSharp.fluid}
         alt="alt text tk"
-        className="blogImage"
+        className={styles.blogImage}
       />
       <div>
         <h2>
           <Link to={props.blogSlug}>{props.blogTitle}</Link>
         </h2>
-        <p className="blogDate">{props.blogDate}</p>
+        <p className={styles.blogDate}>{props.blogDate}</p>
         <p>
           {props.blogIntro} <Link to={props.blogSlug}> >> read more</Link>
         </p>
@@ -34,7 +34,7 @@ const BlogPage = ({ data }) => (
   <Layout>
     <SEO title="Blog" />
     <h1>Blog</h1>
-    <section className="blogContainer">
+    <section className={styles.blogContainer}>
       <BlogEntry
         blogDate="2020-04-05"
         blogTitle="Latest Article Title"
@@ -42,7 +42,7 @@ const BlogPage = ({ data }) => (
         blogSlug="20_02_05-article01"
         blogImage={data.placeholder}
         featured={true}
-        className="blogEntry"
+        className={styles.blogEntry}
       />
       <BlogEntry
         blogDate="2020-03-15"
@@ -50,7 +50,7 @@ const BlogPage = ({ data }) => (
         blogIntro="Sed sit amet arcu finibus, venenatis nunc id, sodales est. Duis vehicula dolor arcu, nec pretium nibh ultrices eu. Curabitur non dapibus augue. "
         blogSlug="20_02_05-article02"
         blogImage={data.placeholder}
-        className="blogEntry"
+        className={styles.blogEntry}
       />
       <BlogEntry
         blogDate="2020-03-02"
@@ -58,7 +58,7 @@ const BlogPage = ({ data }) => (
         blogIntro="Duis lacinia accumsan libero. Duis vehicula dolor arcu, nec pretium nibh ultrices eu. Curabitur non dapibus augue. Sed efficitur justo sit amet erat rhoncus, in eleifend massa accumsan."
         blogSlug="20_02_05-article03"
         blogImage={data.placeholder}
-        className="blogEntry"
+        className={styles.blogEntry}
       />
       <BlogEntry
         blogDate="2020-02-05"
@@ -66,7 +66,7 @@ const BlogPage = ({ data }) => (
         blogIntro="Duis lacinia accumsan libero. Sed sit amet arcu finibus, venenatis nunc id, sodales est. Duis vehicula dolor arcu, nec pretium nibh ultrices eu. Curabitur non dapibus augue. Sed efficitur justo sit amet erat rhoncus, in eleifend massa accumsan."
         blogSlug="20_02_05-article04"
         blogImage={data.placeholder}
-        className="blogEntry"
+        className={styles.blogEntry}
       />
       <BlogEntry
         blogDate="2020-01-16"
@@ -74,7 +74,7 @@ const BlogPage = ({ data }) => (
         blogIntro="Duis vehicula dolor arcu, nec pretium nibh ultrices eu. Curabitur non dapibus augue. Sed efficitur justo sit amet erat rhoncus, in eleifend massa accumsan."
         blogSlug="20_02_05-article05"
         blogImage={data.placeholder}
-        className="blogEntry"
+        className={styles.blogEntry}
       />
     </section>
   </Layout>
